caaefd69f277ec8c53880bc7fd60fb83ef635755
Matias <guerramatias1409@gmail.com>
2021-06-07 12:43:19 -0300

    First Commit - Mostrar mapa y primeros markers

---
+++ lib/main.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  LatLng position = LatLng(-34.58720957992827, -58.64496244855623);
+  MapType mapType = MapType.normal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Stack(
+          children: [
+            GoogleMap(
+              mapType: mapType,
+              initialCameraPosition: CameraPosition(
+                target: position,
+                zoom: 11,
+              ),
+              markers: {
+                Marker(
+                  markerId: MarkerId(position.toString()),
+                  position: position,
+                  alpha: 0.8, //Opacidad del marcador
+                  anchor: const Offset(0.2, 0.2), //Mueve el marker hacia algun lado
+                  draggable: true, //Permite mover el marker haciendo click
+                  onDragEnd: onDragEnd, //Metodo que se dispara al terminar de mover el marker
+                  zIndex: 2 //Indice que marca si quedara encima o debajo de otros markers
+                ),
+                Marker(
+                  markerId: MarkerId('2'),
+                  position: LatLng(-34.60947687154119, -58.76228302717209),
+                  alpha: 0.8, //Opacidad del marcador
+                  anchor: const Offset(0.2, 0.2), //Mueve el marker hacia algun lado
+                  draggable: true, //Permite mover el marker haciendo click
+                  onDragEnd: onDragEnd, //Metodo que se dispara al terminar de mover el marker
+                  zIndex: 1
+                )
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 8, right: 8),
+              child: SpeedDial(
+                animatedIcon: AnimatedIcons.menu_close,
+                overlayColor: Colors.black,
+                overlayOpacity: 0.5,
+                elevation: 8.0,
+                children: [
+                  SpeedDialChild(
+                    label: 'Normal',
+                    child: Icon(Icons.room),
+                    onTap: () => setState(() => mapType = MapType.normal),
+                  ),
+                  SpeedDialChild(
+                    label: 'Satelite',
+                    child: Icon(Icons.satellite),
+                    onTap: () => setState(() => mapType = MapType.satellite),
+                  ),
+                  SpeedDialChild(
+                    label: 'Hibrido',
+                    child: Icon(Icons.compare),
+                    onTap: () => setState(() => mapType = MapType.hybrid),
+                  ),
+                  SpeedDialChild(
+                    label: 'Terreno',
+                    child: Icon(Icons.terrain),
+                    onTap: () => setState(() => mapType = MapType.terrain),
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
+  }
+
+  void onDragEnd(LatLng position){
+    print('new position $position');
+  }
+}
